beb0a6e1fa5f1c6f055b9ce8dcb26dff817da19a
slashdevo <37122880+slashdevo@users.noreply.github.com>
2018-11-09 19:54:11 +0200

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/OOP_Project.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/devassassin/CLionProjects/OOP_Project/Bullet.cpp" "/home/devassassin/CLionProjects/OOP_Project/cmake-build-debug/CMakeFiles/OOP_Project.dir/Bullet.cpp.o"
+  "/home/devassassin/CLionProjects/OOP_Project/Game_Manager.cpp" "/home/devassassin/CLionProjects/OOP_Project/cmake-build-debug/CMakeFiles/OOP_Project.dir/Game_Manager.cpp.o"
+  "/home/devassassin/CLionProjects/OOP_Project/Player.cpp" "/home/devassassin/CLionProjects/OOP_Project/cmake-build-debug/CMakeFiles/OOP_Project.dir/Player.cpp.o"
+  "/home/devassassin/CLionProjects/OOP_Project/UI.cpp" "/home/devassassin/CLionProjects/OOP_Project/cmake-build-debug/CMakeFiles/OOP_Project.dir/UI.cpp.o"
+  "/home/devassassin/CLionProjects/OOP_Project/main.cpp" "/home/devassassin/CLionProjects/OOP_Project/cmake-build-debug/CMakeFiles/OOP_Project.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/OOP_Project.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OOP_Project.dir/main.cpp.o"
+  "CMakeFiles/OOP_Project.dir/Player.cpp.o"
+  "CMakeFiles/OOP_Project.dir/Game_Manager.cpp.o"
+  "CMakeFiles/OOP_Project.dir/Bullet.cpp.o"
+  "CMakeFiles/OOP_Project.dir/UI.cpp.o"
+  "OOP_Project.pdb"
+  "OOP_Project"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OOP_Project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
